4461ec46fd811e35fa2771b9a8aeac09e48fd4b0
Nebula4869 <nebula4869@foxmail.com>
2020-11-28 11:01:30 +0800

    add files

---
+++ README.md
@@ -0,0 +1,209 @@
+# YOLOv5 ncnn-ARM
+An ncnn implementation of YOLOv5 on ARM devices, capable of using GPU to accelerate inference
+
+### Environment
+
+- Ubuntu 18.04 (x86_64)
+- Ubuntu 16.04 (aarch64)
+- OpenCV 3.2.0
+- CMake 3.10.0
+
+### Getting Started with CPU version
+
+1. The compilation of the project should be on the ARM device.
+
+2. Install OpenCV.
+
+   ```shell
+   sudo apt-get install libopencv-dev
+   ```
+
+3. ```
+   cd YOLOv5ncnn
+   ```
+
+4. Edit "CMakeLists.txt" to configure correctly.
+
+5. Compile and run.
+
+   ```shell
+   cd build
+   cmake ..
+   make
+   ./../bin/YOLOv5ncnn
+   ```
+
+### Compile ncnn-ARM by yourself
+
+1. The compilation of ncnn should be on the x86 device.
+
+2. Install OpenCV.
+
+   ```shell
+   sudo apt-get install libopencv-dev
+   ```
+
+3. Install protobuf.
+
+   ```shell
+   sudo apt install protobuf-compiler libprotobuf-dev 
+   ```
+
+4. Download source code of ncnn from https://github.com/Tencent/ncnn/releases.
+
+   ```shell
+   unzip ncnn-master.zip
+   ```
+
+5. Download [gcc-arm-toolchain](https://developer.arm.com/-/media/Files/downloads/gnu-a/8.2-2018.11/gcc-arm-8.2-2018.11-x86_64-aarch64-linux-gnu.tar.xz?revision=7a60a425-1aa0-43f5-b9db-1af71bffadc6&la=en) and add to environment variables.
+
+   ```shell
+   tar -zxvf gcc-arm-8.2-2018.11-x86_64-aarch64-linux-gnu.tar.xz
+   gedit ~/.bashrc
+   export PATH=$PATH:/home/username/gcc-arm-8.2-2018.11-x86_64-aarch64-linux-gnu/bin
+   source ~/.bashrc
+   ```
+
+6. Compile ncnn.
+
+   ```shell
+   cd ncnn
+   mkdir -p build-aarch64-linux
+   cd build-aarch64-linux
+   cmake -DCMAKE_TOOLCHAIN_FILE=../toolchains/aarch64-linux-gnu.toolchain.cmake ..
+   make -j8
+   make install
+   ```
+
+### Getting Started with GPU version
+
+1. The compilation of the project should be on the ARM device.
+
+2. Install OpenCV.
+
+   ```shell
+   sudo apt-get install libopencv-dev
+   ```
+
+3. ```
+   cd YOLOv5ncnn-vulkan
+   ```
+
+4. Edit "CMakeLists.txt" to configure correctly.
+
+5. Compile and run.
+
+   ```shell
+   cd build
+   cmake ..
+   make
+   ./../bin/YOLOv5ncnn-vulkan
+   ```
+
+### Compile ncnn-vulkan-ARM by yourself
+
+1. The compilation of ncnn-vulkan should be on the x86 device.
+
+2. Install protobuf.
+
+   ```shell
+   sudo apt install protobuf-compiler libprotobuf-dev 
+   ```
+
+3. Install OpenCV.
+
+   ```shell
+   sudo apt-get install libopencv-dev
+   ```
+
+4. Download vulkan-sdk from https://vulkan.lunarg.com/sdk/home#sdk/downloadConfirm/1.2.148.0/linux/vulkansdk-linux-x86_64-1.2.148.0.tar.gz. and add to environment variables (reboot may needed).
+
+   ```shell
+   export VULKAN_SDK=~/vulkan-sdk-1.2.148.0/x86_64
+   export PATH=$PATH:$VULKAN_SDK/bin
+   export LIBRARY_PATH=$LIBRARY_PATH$:VULKAN_SDK/lib
+   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$VULKAN_SDK/lib
+   export VK_LAYER_PATH=$VULKAN_SDK/etc/vulkan/explicit_layer.d
+   ```
+
+5. Download source code of ncnn from https://github.com/Tencent/ncnn/releases.
+
+   ```shell
+   unzip ncnn-master.zip
+   ```
+
+6. Download [gcc-arm-toolchain](https://developer.arm.com/-/media/Files/downloads/gnu-a/8.2-2018.11/gcc-arm-8.2-2018.11-x86_64-aarch64-linux-gnu.tar.xz?revision=7a60a425-1aa0-43f5-b9db-1af71bffadc6&la=en) and add to environment variables.
+
+   ```shell
+   tar -zxvf gcc-arm-8.2-2018.11-x86_64-aarch64-linux-gnu.tar.xz
+   gedit ~/.bashrc
+   export PATH=$PATH:/home/username/gcc-arm-8.2-2018.11-x86_64-aarch64-linux-gnu/bin
+   source ~/.bashrc
+   ```
+
+7. Compile ncnn.
+
+   ```shell
+   cd ncnn
+   mkdir -p build-aarch64-linux-vulkan
+   cd build-aarch64-linux-vulkan
+   cmake -DNCNN_VULKAN=ON -DCMAKE_TOOLCHAIN_FILE=../toolchains/aarch64-linux-gnu.toolchain.cmake ..
+   make -j8
+   make install
+   ```
+
+8. In order to compile the project correctly on ARM devices, additional static link libraries (libvulkan-sdk.a  and libvulkan-stub.a) are needed and can be obtained from [here]([ARM-software/vulkan-sdk: Github repository for the Vulkan SDK](https://github.com/ARM-software/vulkan-sdk)).
+
+### Get your own Yolov5 ncnn model
+
+We train a model in Pytorch and first convert to onnx and then to ncnn.
+
+1. For how to train in Pytorch and export to onnx, see https://github.com/ultralytics/yolov5.
+
+2. Because ncnn has limited support for operators, the network definition needs to be modified before training, please modify "common.py".
+
+   from
+
+   ```python
+   class Focus(nn.Module):
+       def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):
+           super(Focus, self).__init__()
+           self.conv = Conv(c1 * 4, c2, k, s, p, g, act)
+   
+       def forward(self, x):
+           return self.conv(torch.cat([x[..., ::2, ::2], x[..., ::2, ::2], x[..., ::2, ::2], x[..., ::2, ::2]], 1))
+   ```
+
+   to
+
+   ```python
+   class Focus(nn.Module):
+       def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):
+           super(Focus, self).__init__()
+           self.conv = Conv(c1 * 4, c2, k, s, p, g, act)
+   
+       def forward(self, x):
+           return self.conv(torch.cat([torch.nn.functional.interpolate(x, scale_factor=0.5),
+                                       torch.nn.functional.interpolate(x, scale_factor=0.5),
+                                       torch.nn.functional.interpolate(x, scale_factor=0.5),
+                                       torch.nn.functional.interpolate(x, scale_factor=0.5)], 1))
+   ```
+
+3. When export to onnx, Detect layer should be removed from the graph, please modify  "export.py".
+
+   ```python
+   model.model[-1].export = True
+   ```
+
+4. Simplify the onnx model by onnx-simplifier.
+
+   ```shell
+   pip3 install onnx-simplifier
+   python3 -m onnxsim yolov5s.onnx yolov5s.onnx
+   ```
+
+5. Convert onnx to ncnn
+
+   ```shell
+   ./onnx2ncnn yolov5s.onnx yolov5s.param yolov5s.bin
+   ```
---
+++ YOLOv5ncnn/src/YOLOv5ncnn.cpp
@@ -0,0 +1,184 @@
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <algorithm>
+#include <vector>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include "ncnn/net.h"
+
+using namespace std;
+
+
+const int numCLasses = 80;
+const int frameWidth = 1920;
+const int frameHeight = 1080;
+
+vector<vector<vector<int>>> anchors{{{10, 13}, {16, 30}, {33, 23}},
+                                                                               {{30, 61}, {62, 45}, {59, 119}},
+                                                                               {{116, 90}, {156, 198}, {373, 326}}};
+vector<vector<float>> dets;
+
+inline float sigmoid(float x){
+    return 1.0f / (1.0f + exp(-x));
+}
+
+void decodeResult(const ncnn::Mat& data, int stride, vector<vector<int>> anchors, float scoreThresh)
+{
+    for (int c=0; c<data.c; c++)
+    {
+        const float* ptr = data.channel(c);
+        for (int y=0; y<data.h; y++)
+        {
+            float score = sigmoid(ptr[4]);
+            if (score > scoreThresh)
+            {
+                vector<float> det(6);
+                det[1] = (sigmoid(ptr[0] )* 2 - 0.5 + y % (int)(640 / stride)) * stride * frameWidth / 640; //center_x
+                det[2] = (sigmoid(ptr[1]) * 2 - 0.5 + (int)(y / (640 / stride))) * stride * frameHeight / 384; //center_y
+                det[3] = pow((sigmoid(ptr[2]) * 2), 2) * anchors[c][0] * frameWidth / 640; //w
+                det[4] = pow((sigmoid(ptr[3]) * 2), 2) * anchors[c][1] * frameHeight / 384; //h
+
+                det[1] = det[1] - det[3] / 2; //left
+                det[2] = det[2] - det[4] / 2; //top
+                det[3] = det[1] + det[3]; //right
+                det[4] = det[2] + det[4]; //bottom
+
+                for (int i=5; i<numCLasses+5; i++)
+                {
+                    float conf = sigmoid(ptr[i]);
+                    if (conf * score > det[0])
+                    {
+                        det[0] = conf * score; //score
+                        det[5] = i - 5; //class_id
+                    }
+                }
+                dets.push_back(det);
+            }
+            ptr += data.w;
+        }
+    }
+}
+
+void nonMaxSuppression(float iouThresh)
+{
+    int length = dets.size();
+    int index = length - 1;
+
+    sort(dets.begin(), dets.end());
+    vector<float> areas(length);
+    for (int i=0; i<length; i++)
+    {
+        areas[i] = (dets[i][4] - dets[i][2]) * (dets[i][3] - dets[i][1]);
+    }
+    
+    while (index  > 0)
+    {
+        int i = 0;
+        while (i < index)
+        {
+            float left = max(dets[index][1], dets[i][1]);
+            float top = max(dets[index][2], dets[i][2]);
+            float right = min(dets[index][3], dets[i][3]);
+            float bottom = min(dets[index][4], dets[i][4]);
+            float overlap = max(0.0f, right - left) * max(0.0f, bottom - top);
+            if (overlap / (areas[index] + areas[i] - overlap) > iouThresh)
+            {
+                areas.erase(areas.begin() + i);
+                dets.erase(dets.begin() + i);
+                index --;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        index--;
+    }
+}
+
+int main()
+{
+    std::vector<std::string> classnames;
+	std::ifstream f("../coco.names");
+	std::string name = "";
+	while (std::getline(f, name))
+	{
+		classnames.push_back(name);
+    }
+
+    ncnn::Net net;
+    net.opt.num_threads=12;
+    net.load_param("../yolov5s.param");
+    net.load_model("../yolov5s.bin");
+    ncnn::Mat input, output0, output1, output2;
+
+    cv::Mat frame, img;
+    cv::VideoCapture cap = cv::VideoCapture(0);
+    cap.set(CV_CAP_PROP_FRAME_WIDTH, frameWidth);
+    cap.set(CV_CAP_PROP_FRAME_HEIGHT, frameHeight);
+    while(cap.isOpened())
+    {
+        const auto t0 = std::chrono::system_clock::now();
+	    cap.read(frame);
+        if(frame.empty())
+        {
+           std::cout << "Read frame failed!" << std::endl;
+           break;
+        }
+
+        ncnn::Extractor extractor = net.create_extractor();
+        cv::resize(frame, img, cv::Size(640, 384));
+        cv::cvtColor(img, img, cv::COLOR_BGR2RGB);
+
+        const auto t1 = std::chrono::system_clock::now();
+
+        input = ncnn::Mat::from_pixels(img.data, ncnn::Mat::PIXEL_RGB, img.cols, img.rows);
+        float norm[3] = {1/255.f,1/255.f,1/255.f};
+        float mean[3] = {0,0,0};
+        input.substract_mean_normalize(mean,norm);
+        extractor.input("images", input);
+        extractor.extract("output", output0);
+        extractor.extract("424", output1);
+        extractor.extract("444", output2);
+
+        const auto t2 = std::chrono::system_clock::now();
+
+        dets.clear();
+        decodeResult(output0, 8, anchors[0], 0.6);
+        decodeResult(output1, 16, anchors[1], 0.6);
+        decodeResult(output2, 32, anchors[2], 0.6);
+        nonMaxSuppression(0.5);
+
+        const auto t3 = std::chrono::system_clock::now();
+
+        for (int i=0; i<dets.size(); i++)
+        {
+                float left = dets[i][1];
+                float top = dets[i][2];
+                float right = dets[i][3];
+                float bottom = dets[i][4];
+                float score = dets[i][0];
+                int classID = dets[i][5];
+
+				cv::rectangle(frame, cv::Rect(left, top, (right - left), (bottom - top)), cv::Scalar(255, 255, 0), 2);
+
+				cv::putText(frame,
+					classnames[classID] + ": " + cv::format("%.2f", score),
+					cv::Point(left, top),
+					cv::FONT_HERSHEY_SIMPLEX, (right - left) / 200, cv::Scalar(255, 255, 0), 2);
+        }
+        
+        cv::imshow("", frame);
+        if(cv::waitKey(1)== 27) break;
+
+        const auto t4 = std::chrono::system_clock::now();
+
+        cout << "Inference Time: " << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() * 1e-3 << "ms  ";
+        cout << "Post-processing Time: " << std::chrono::duration_cast<std::chrono::microseconds>(t3 - t2).count() * 1e-3 << "ms  ";
+        cout << "Total Time: " << std::chrono::duration_cast<std::chrono::microseconds>(t4 - t0).count() * 1e-3 << "ms" << endl;
+    }
+    return 0;
+}
